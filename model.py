3c24d3aba3f913533affa90248058428c194cb5d
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-21 14:51:26 -0500

    removed visualization

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd 
-from sklearn.linear_model import LinearRegression 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier 
 
 df = pd.read_csv("Heart_disease_statlog.csv") 
 df = df.rename(columns={"target": "class"}) 
-print(df.info())
+
