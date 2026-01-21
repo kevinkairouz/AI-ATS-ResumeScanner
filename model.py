f0c0d39018e5c4257ce39b6331a5aebc51578740
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-21 14:27:33 -0500

    renamed target attribute to class

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression 
 import matplotlib.pyplot as plt 
 
-df = pd.read_csv("Heart_disease_statlog.csv")
-
-plt.scatter(df["age"], df["chol"]) 
-plt.show()
+df = pd.read_csv("Heart_disease_statlog.csv") 
+df = df.rename(columns={"target": "class"}) 
+print(df.info())
