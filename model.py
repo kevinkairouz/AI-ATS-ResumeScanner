7f602e3ad7cd79a915078c757890abdbd59de41c
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-21 14:24:07 -0500

    imported csv and created scatterplot

--- model.py
+++ model.py
@@ -2,4 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression 
 import matplotlib.pyplot as plt 
 
+df = pd.read_csv("Heart_disease_statlog.csv")
 
+plt.scatter(df["age"], df["chol"]) 
+plt.show()
