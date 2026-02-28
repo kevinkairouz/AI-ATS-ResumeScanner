f6fb9f1f9eab5c568aa66848c3f6561ec861dd2c
kevinkairouz <KM.Kairouz@gmail.com>
2026-02-28 15:06:26 -0500

    removed irrelevant files

--- model.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier 
-
-df = pd.read_csv("Heart_disease_statlog.csv") 
-df = df.rename(columns={"target": "class"}) 
-
