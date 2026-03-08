78344f738c8af0d6f31ecd183a02be32f04934fa
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-07 23:20:20 -0500

    renamed file & finished class implementation

--- resume.py
+++ resume.py
@@ -3,7 +3,6 @@ import pdfplumber as plu
 import numpy as np
 
 class ResumeManager:
-
     def __init__(self, resume_file):
         self.resume_file = resume_file
 
@@ -13,9 +12,14 @@ class ResumeManager:
             for page in resume.pages:  
                 file_info += page.extract_text_simple() 
             return file_info 
+    
+    def convertToDF(self, pdf_string): 
+        df = pd.DataFrame({"Resume": [pdf_string]}) 
+        return df 
 
-
-
+    def concatDF(self, df1, df2): 
+        df_concat = pd.concat([df1,df2]) 
+        return df_concat
 
 
 
--- test.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber as pdp 
-
-
-with pdp.open("sample.pdf") as pdf: 
-    firstPage = pdf.pages[0]
-    text = firstPage.extract_text() 
-    print(text)
