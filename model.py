1cb9f8335990858d4c734480ed0421e6640a2bc3
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-07 21:21:45 -0500

    creating func to getText from a pdf

--- model.py
+++ model.py
@@ -2,11 +2,8 @@ import pandas as pd
 from pypdf import PdfReader  
 import numpy as np
 
+def getText(pdf): 
 
-reader = PdfReader("sample.pdf", strict=True)
 
-pages = reader.pages
 
-for i in range(len(pages)): 
-    if 
 
