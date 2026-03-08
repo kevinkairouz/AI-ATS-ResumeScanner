c91d767b20c267af327baa62f8cd11362697c426
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-08 01:21:04 -0500

    startedcreating models for V1 dataset

--- resume.py
+++ resume.py
@@ -2,15 +2,23 @@ import pandas as pd
 import pdfplumber as plu
 
 class ResumeManager:
-    def __init__(self, resume_file):
-        self.resume_file = resume_file
-
+    
+    def __init__(self):
+        pass
     def getText(self, pdf): 
-        with plu.open(pdf) as resume: 
+        with plu.open(f"resumes/{pdf}") as resume:  
             file_info = ""
             for page in resume.pages:  
                 file_info += page.extract_text_simple() 
-            return file_info 
+
+    def prepareDF(self): 
+        """
+        This will do loop for all of the sample resumes and get all of the text 
+        run the getText function 28 times
+        """
+        return None 
+
+             
     
     def convertToDF(self, pdf_string): 
         df = pd.DataFrame({"Resume": [pdf_string], "class": []}) 
@@ -20,13 +28,7 @@ class ResumeManager:
         df_concat = pd.concat([df1,df2]) 
         return df_concat
 
-
-
-
-
-    
-
-
+r = ResumeManager() 
 
 
 
