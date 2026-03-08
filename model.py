bb040a7b7f33796839672e6373bdc40284fa782a
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-07 21:07:23 -0500

    imported libraries for pdf parsing

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 import pandas as pd 
+from pypdf import PdfReader  
+import numpy as np
+
+
+reader = PdfReader("sample.pdf", strict=True)
+
+pages = reader.pages
+
+for i in range(len(pages)): 
+    if 
+
