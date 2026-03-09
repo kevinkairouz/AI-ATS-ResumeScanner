e5b943edbf91b6f4d894765b659eb329a3fb29e8
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-09 15:48:43 -0400

    parameter tuned model and cleaned data

--- analysis.py
+++ analysis.py
@@ -8,6 +8,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split 
 from sklearn.model_selection import GridSearchCV  
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 df = pd.read_csv("datasets/Resume.csv")
 df = df.drop(["ID","Resume_html"], axis=1) 
@@ -15,7 +17,9 @@ df = df.drop(["ID","Resume_html"], axis=1)
 rows_to_drop = df[(df["Category"] == "ADVOCATE") | (df["Category"] == "AGRICULTURE") | (df["Category"] == "APPAREL") | (df["Category"] == "AUTOMOBILE") | 
                   (df["Category"] == "AVIATION") | (df["Category"] == "BPO") | (df["Category"] == "CHEF") |
                   (df["Category"] == "CONSTRUCTION") | (df["Category"] == "DESIGNER") | (df["Category"] == "FITNESS") |
-                  (df["Category"] == "HEALTHCARE") | (df["Category"] == "TEACHER") | (df["Category"] == "CONSULTANT")]  
+                  (df["Category"] == "HEALTHCARE") | (df["Category"] == "TEACHER") | (df["Category"] == "CONSULTANT") 
+                  | (df["Category"] == "BANKING") | (df["Category"] == "ACCOUNTANT") | (df["Category"] == "DIGITAL-MEDIA") | (df["Category"] == "ARTS")
+                  ]  
 
 df = df.drop(rows_to_drop.index, axis=0)
 print(df.groupby("Category").count()) 
@@ -23,16 +27,21 @@ print(df.groupby("Category").count())
 X = df["Resume_str"] 
 Y = df["Category"] 
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2, stratify=Y, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,random_state=42)
 
-tf = TfidfVectorizer(stop_words="english", max_features=2650, ngram_range=(1,2)) 
+tf = TfidfVectorizer(stop_words="english", max_features=5000,ngram_range=(1,2)) 
 cv = CountVectorizer(stop_words="english", max_features=2600)
 X_train_data = tf.fit_transform(X_train) 
 X_test_data = tf.transform(X_test)  
 
-model = RandomForestClassifier()
+model = RandomForestClassifier(n_estimators = 500, max_depth = 30) 
 
+model.fit(X_train_data, Y_train)  
+Ypred = model.predict(X_test_data)
+print(classification_report(Y_test,Ypred))
 
-model.fit(X_train_data, Y_train) 
 print(model.score(X_test_data,Y_test))
+
+
+
 
