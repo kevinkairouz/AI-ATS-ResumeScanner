ab5716f59ca98f76a11119902aef3b7094521781
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-09 01:45:40 -0400

    cleaned data for accuracy/clarification purpose

--- analysis.py
+++ analysis.py
@@ -14,6 +14,15 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 
 df = pd.read_csv("datasets/UpdatedResume.csv")
+rows_to_delete = df[(df["Category"] == "Advocate") | (df["Category"] == "BlockChain") | (df["Category"] == "Hadoop") | (df["Category"] == "PMO") | (df["Category"] == "Testing")] 
+df = df.drop(rows_to_delete.index, axis=0)  
+
+location1 = df[df["Category"] == "DotNet Developer"].index  
+location2 = df[df["Category"] == "ETL Developer"].index
+location3 = df[df["Category"] == "Network Security Engineer"].index
+df.loc[location1, "Category"] = "C# Developer" 
+df.loc[location2, "Category"] = "Data Engineer" 
+df.loc[location3, "Category"] = "Cybersecurity"
 print(df.groupby("Category").count())
 
 
