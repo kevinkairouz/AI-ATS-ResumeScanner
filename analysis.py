6b8d8a0c8cf806a290510aaddb13cc9b3780d4bf
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-08 22:39:02 -0400

    updated csv and deleting irrelevant categories

--- analysis.py
+++ analysis.py
@@ -8,19 +8,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.metrics import recall_score 
 from sklearn.metrics import recall_score
-from sklearn.feature_extraction.text import CountVectorizer 
 from sklearn.feature_extraction.text import TfidfVectorizer  
 from sklearn.model_selection import train_test_split 
 from sklearn.model_selection import GridSearchCV  
 from sklearn.model_selection import RandomizedSearchCV
 
-df = pd.read_csv("datasets/resumes.csv") 
+df = pd.read_csv("datasets/UpdatedResume.csv")
+print(df.groupby("Category").count())
 
-X = df["Resume"] 
-Y = df["Category"] 
-cv = CountVectorizer(stop_words="english") 
-tf = TfidfVectorizer(stop_words="english") 
-X = cv.fit_transform(X) 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, random_state=42, test_size=0.3) 
 
 
