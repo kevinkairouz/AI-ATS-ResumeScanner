f3430ccfc145bb9edbee6530e41427e2ed8abfca
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-12 02:40:00 -0400

    wrote outline for scoring methods of how the logic will be implemented potientially

---
+++ model/score.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+"""
+
+will have stuff for business to see if it is a great fit and if yes the 
+person has the tools, HR can get resume 
+
+same type of logic applies for other roles in tech
+""" 
