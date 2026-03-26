48fc5d88814ddade7575ee9e6a4a041b65733938
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-26 14:45:17 -0400

    created nontechrole implementation

--- model/other_role.py
+++ model/other_role.py
@@ -1,9 +1,11 @@
 #targeted for non-tech roles such as HR, Operations, etc 
 
-def good_fit(resume): 
-    resume = str(resume) 
-    resume = resume.upper()
-    if "MASTERS" in resume or "EXCEL" in resume: 
-        return True 
-    else: 
-        return False
+
+class NonTechRoleManager: 
+    def good_fit(resume): 
+        resume = str(resume) 
+        resume = resume.upper()
+        if "MASTERS" in resume or "EXCEL" in resume: 
+            return True 
+        else: 
+            return False
