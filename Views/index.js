90e139f41652e85ec9924c421bb8f109f2698d21
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-26 16:17:52 -0400

    added mysql functionality with connector and db design

--- Views/index.js
+++ Views/index.js
@@ -1,5 +1,22 @@
-function findFit(){ 
-    fetch
-    
+function findFit() {
+    const file = document.getElementById("resumeInput").files[0];
+    if (!file) {
+        console.log("No file selected");
+        return;
+    }
 
+    const formData = new FormData();
+    formData.append("file", file);
+
+    fetch("http://127.0.0.1:5000/uploadResume", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log("Something went wrong");
+    });
 }
--- app.py
+++ app.py
@@ -1,59 +1,79 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from model.resume import ResumeManager 
 from model.category import CategoryManager 
 from model.role import TechRoleManager 
 from model.score import ScoreManager 
-from model.other_role import NonTechRoleManager
+from model.other_role import NonTechRoleManager 
+import mysql.connector as sql
+
 
         
-app = Flask(__name__)  
+app = Flask(__name__)   
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+r_m = ResumeManager()
 
 def predict(resume):  
-    rmanager = ResumeManager() 
-    cmanager = CategoryManager() 
-    tmanager = TechRoleManager() 
-    smanager = ScoreManager() 
-    omanager = NonTechRoleManager() 
+    r = ResumeManager() 
+    c = CategoryManager() 
+    t = TechRoleManager() 
+    s = ScoreManager() 
+    o = NonTechRoleManager()  
+    tech_roles = ["BUSINESS","ENGINEERING","FINANCE","INFORMATION-TECHNOLOGY"]
+
+
 
     #TODO include score and nontechroleManager we may changed ScoreManagerNonTech 
 
-    text = rmanager.getText(resume)  
-    classification = cmanager.makePrediction(text) 
-    if classification == "INFORMATION-TECHNOLOGY" or classification == "BUSINESS" or classification == "ENGINEERING": 
-        role = tmanager.predictRole(text) 
-        return role        
-    else:
-
-        return classification
+    text = r.getText(resume)  
+    classification = c.makePrediction(text) 
+    message = send_data(text, classification)
+    if classification in tech_roles: 
+        projxon_role = t.predictRole(text) 
+        return s.isFit(text,projxon_role)
+    else:  
+        return o.is_Fit(text, classification) 
     
-r_m = ResumeManager()
+
+def send_data(resume, classification): 
+    inserted_data = (resume, classification)
+    db = sql.connect(root = "user", password = "Dominics1", host = "localhost", database = "resumes") 
+    cursor = db.cursor() 
+    cursor.execute("insert into applicants values (%s, %s)")
+    db.commit() 
+    return "COMPLETED DB TRANSACTION"
 
 @app.route("/") 
+@cross_origin()
 def greet(): 
     return "Hello There!!" 
 
 @app.route("/uploadResume", methods = ["POST"]) 
+@cross_origin()
 def upload():          
         if "file" in request.files: 
             pdf = request.files["file"] 
             if pdf.filename == "" or r_m.getText(pdf) == "": 
                 return "JUNK FILE"
             else:       
-                role = predict(pdf) 
-                return jsonify({"ROLE": role}) 
+                res = predict(pdf) 
+                return jsonify({"ROLE": res}) 
         else: 
             return "No file yet"
 
-@app.route("/uploadResumes", methods = ["POST"]) 
-def uploadResumes(): 
-    files = request.args.getlist("file") 
-    roles = [] 
-    for file in files: 
-        role = predict(file) 
-        roles.append(role) 
-    return jsonify({"ROLES": roles})
+app.run(debug=True)
+#V1 will not include a list of resumes TBD feature
+# @app.route("/uploadResumes", methods = ["POST"])
+# @cross_origin()
+# def uploadResumes(): 
+#     files = request.args.getlist("file") 
+#     roles = [] 
+#     for file in files: 
+#         role = predict(file) 
+#         roles.append(role) 
+#     return jsonify({"ROLES": roles})
         
         
-app.run()
+# app.run()
 
 
