e53be243d8a013a2398150046312416b72a8a696
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-28 22:34:28 -0400

    created applicant class to keep track of candidates

--- model/category.py
+++ model/category.py
@@ -9,7 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV  
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import classification_report  
-from sklearn.ensemble import GradientBoostingClassifier 
+from sklearn.ensemble import GradientBoostingClassifier  
+from dataclasses import dataclass 
+
 
 #V2 model will use the following new dataset (dataset.txt) 
 #For App.py we may have to change the filepath
@@ -25,7 +27,7 @@ df = df.loc[records_keep]
 
 
 
-#Lowering Entropy 
+#Lowering Entropy uncertainity in the data 
 is_Cyber = df[(df["Category"] == "Network Security Engineer") | (df["Category"] == "Blockchain")].index  
 is_TL = df[df["Category"] == "Management"].index 
 
@@ -40,21 +42,12 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,stratify=Y
 tf = TfidfVectorizer(stop_words="english",max_features=3000,ngram_range=(1,2)) 
 X_train_data = tf.fit_transform(X_train) 
 X_test_data = tf.transform(X_test)  
-
-
-
 model = GradientBoostingClassifier(n_estimators=250, learning_rate=1.0, max_depth=7)  
 model.fit(X_train_data, Y_train)
-print(model.score(X_test_data, Y_test)) 
+# print(model.score(X_test_data, Y_test)) 
 
 #after training will make the selections using the prob inside of the predict function and itr 
 #over df and we can make another class that is record 
-
-
-
-
-
-
 
 # model = RandomForestClassifier(n_estimators=400, max_depth=100) 
 # model.fit(X_train_data, Y_train)  
@@ -65,19 +58,28 @@ print(model.score(X_test_data, Y_test))
 # print(classification_report(Y_test, Y_predicted)) 
 
 
-#Gradient Boosting Benchmarked at 92.5% accuracy as of March 28th
+#Gradient Boosting Benchmarked at 92.5% accuracy as of March 28th 
+
+@dataclass
+class Applicant:  
+    Resume: str  
+    PrimaryFit: str 
+    Secondary1: str 
+    Secondary2: str 
+    score1: float 
+    score2: float 
+    score3: float
+
+
 
 class CategoryManager: 
     def makePrediction(self, text):   
         data = tf.transform([text])  
-        y_predict_prba = model.predict_proba(data)  
+        y_predict_prba = model.predict_proba(data) 
+        df = pd.DataFrame("Classes")
         
 
     
-    def displaySampleReport(self): 
-        res = self.makePrediction(Y_test)
-        print(classification_report(Y_test,res))
-        return None
      
 
 
