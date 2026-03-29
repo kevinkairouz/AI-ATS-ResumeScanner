23cda2ed1cd8ca9e2e065aee601d4c282a0157bb
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-29 14:56:14 -0400

    started rewriting backend api and will implement blueprints for organization between excel, model, and database

--- model/category.py
+++ model/category.py
@@ -37,10 +37,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,stratify=Y
 tf = TfidfVectorizer(stop_words="english",max_features=3000,ngram_range=(1,2)) 
 X_train_data = tf.fit_transform(X_train) 
 X_test_data = tf.transform(X_test)  
-# model = GradientBoostingClassifier(n_estimators=250, learning_rate=1.0, max_depth=7) 
+# model2 = GradientBoostingClassifier(n_estimators=250, learning_rate=1.0, max_depth=7) 
 model = RandomForestClassifier(n_estimators=400, max_depth=100)
 model.fit(X_train_data, Y_train)
-# print(model.score(X_test_data, Y_test)) 
+# print(model.score(X_test_data, Y_test))  
+#print(model2.score(X_test_data, Y_test))
 
 #after training will make the selections using the prob inside of the predict function and itr 
 #over df and we can make another class that is record 
@@ -73,14 +74,13 @@ class CategoryManager:
         data = tf.transform([text])  
         y_predict_prba = model.predict_proba(data)
         df = pd.DataFrame({"Classes": model.classes_, "Match": y_predict_prba[0].round(3) * 100}) 
-        df = df.sort_values(by="Match", ascending=False) 
-        df = df[0:3] 
-        print(df)
+        df = df.sort_values(by="Match", ascending=False)  
+        # print(df)
         return Applicant(text, df["Classes"].iloc[0], df["Classes"].iloc[1], df["Classes"].iloc[2], df["Match"].iloc[0], df["Match"].iloc[1], df["Match"].iloc[2])
 
-#for testing purpose
-c = CategoryManager() 
-c.makePrediction(X_test.iloc[4])
+# for testing purpose
+# c = CategoryManager() 
+# c.makePrediction(X_test.iloc[4])
         
 
     
--- model/test.py
+++ model/test.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
 #for testing pandas syntax
-df = pd.DataFrame({"Name": ["Bart", "Check"], "Salary": [100,200]}) 
-
-print(df[0:1])
+df = pd.DataFrame({"Name": ["Bart", "Derek"], "Salary": [100,200]}) 
+df = df.sort_values(by="Salary", ascending=False)
+print(df.iloc[0])
