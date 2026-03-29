572dd2221f4ac7b58d0f2760327acc05c26bd1ac
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-29 15:33:36 -0400

    restructured file aretechuire

--- model/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-#for testing pandas syntax
-df = pd.DataFrame({"Name": ["Bart", "Derek"], "Salary": [100,200]}) 
-df = df.sort_values(by="Salary", ascending=False)
-print(df.iloc[0])
