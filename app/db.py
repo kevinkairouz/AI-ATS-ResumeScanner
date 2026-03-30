2890abc855e17f5a1e685f0949349906f2ac8852
kevinkairouz <KM.Kairouz@gmail.com>
2026-03-29 20:27:21 -0400

    created endpoint for send info

--- app/db.py
+++ app/db.py
@@ -4,4 +4,12 @@ from flask import Flask, Blueprint
 from flask_cors import CORS, cross_origin 
 
 
-db = Blueprint("db")
+db = Blueprint("db", __name__) 
+
+
+db.route("/sendInfo", methods = ["POST"]) 
+def sendInfo(): 
+    query = "insert into resumeSystem.applicants values ()"
+    #this will send data to BigQuery
+
+    return None
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 from flask import Flask, request, jsonify, blueprints, Blueprint
+from app.db import db
+application = Flask(__name__)  
 
-application = Flask(__name__) 
