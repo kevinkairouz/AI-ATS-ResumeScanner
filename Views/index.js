29744c1a0094dbd2feb072e7a1a511e505d8c0f7
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-10 22:35:16 -0400

    revisions to html css and js frontend

--- Views/index.js
+++ Views/index.js
@@ -1,22 +1,4 @@
-function findFit() {
-    const file = document.getElementById("resumeInput").files[0];
-    if (!file) {
-        console.log("No file selected");
-        return;
-    }
+async function findFit(){
+    let response = fetch() 
 
-    const formData = new FormData();
-    formData.append("file", file);
-
-    fetch("http://127.0.0.1:5000/uploadResume", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log("Something went wrong");
-    });
 }
